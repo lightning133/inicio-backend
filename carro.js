b470153760c1220895dada80bf9ead940e1504e0
lightning133 <wesley.rozzi@escola.pr.gov.br>
2026-02-24 07:51:28 -0300

    Add files via upload

---
+++ carro.js
@@ -0,0 +1,11 @@
+carro1="fusca"
+carro2="chevette"
+
+kmcarro1=5;
+kmcarro2=3;
+
+precocarro1=20;
+precocarro2=10;
+Tptalcarro
+
+console.log("o fusca"precocarro1)
---
+++ notas.js
@@ -0,0 +1,7 @@
+aluno="pedro";
+notal1=32;
+nota2=53;
+nota3=90;
+nota4=12;
+média= (notal1+nota2+nota3+nota4/4);
+console.log("o aluno", aluno "tem média",media)
