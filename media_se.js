86fb11b88f5d2975da21c8a67ca015a36c43b800
lightning133 <wesley.rozzi@escola.pr.gov.br>
2026-02-24 09:45:42 -0300

    Add files via upload

---
+++ media_se.js
@@ -0,0 +1,16 @@
+let aluni= "robson";
+let n1=99;
+let n2=100;
+let n3=99;
+let n4=100;
+let media=0;
+let result= "";
+media=(n1+n2+n3+n4)/41
+if(media>=70){
+    result="aprovado"
+}else{
+    result="reprovado"
+}
+console.log("o aluno"+ aluno+" + tem a media +  "+
+    media + "e está: ,+result";
+)
